a318b6a134342a01f98f48c2054ddff1bf50e856
Fabiana <natali@email.arizona.edu>
2022-02-17 13:29:00 -0700

    write out data surveys_complete

--- scripts/01-data-manipulation.R
+++ scripts/01-data-manipulation.R
@@ -75,4 +75,19 @@ surveys %>%
   pivot_wider(names_from  = year,
               values_from = count)
 
-            
+# Exporting filtered data
+surveys_complete <- surveys %>%
+  filter(!is.na(weight),
+         !is.na(hindfoot_length),
+         !is.na(sex))
+
+# Most common species
+species_counts <- surveys_complete %>% 
+  count(species_id) %>%
+  filter(n > 50)
+
+# Only keep the most common species
+surveys_complete <- surveys_complete %>%
+  filter(species_id %in% species_counts$species_id)
+
+write_csv(surveys_complete, file = "data_clean/surveys_complete.csv")
