aa1509a92cf777d02cb6a31eb1fb5e44aac5ffec
KP <k12p96@gmail.com>
2022-02-22 14:20:37 -0700

    for loops

--- scripts/03-if-and-for.R
+++ scripts/03-if-and-for.R
@@ -37,4 +37,60 @@ if (age_class == "sapling"){
 }
 print(y)
 
+#For Loops ----
 
+# for (item in list_of_items) {
+#   do_something(item)
+# }
+
+volumes <- c(1.5, 3, 8)
+for (volume in volumes) {
+  print(2.6 * volume ^ 0.9)
+}
+
+volumes <- c(1.5, 3, 8)
+for (volume in volumes) {
+  mass <- 2.6 * volume ^ 0.9
+  mass_lb <- mass*2.2
+  print(mass_lb)
+}
+
+volumes <- c(1.6, 3, 8)
+for (i in 1:length(volumes)) {
+  mass <- 2.6 * volumes[i] ^ 0.9
+  print(mass)
+}
+
+#empty vecor
+masses <- c()
+#or empty vector of known length
+masses <- c(length = length(volumes))
+for (i in 1:length(volumes)) {
+  mass <- 2.6 * volumes[i] ^ 0.9
+  masses[i] <- mass
+}
+
+masses_2 <- c()
+for (volume in volumes) {
+  mass <- 2.6 * volume ^ 0.9
+  masses_2 <- c(masses_2, mass)
+}
+
+#example
+radius <- c(1.3, 2.1, 3.5)
+areas <- vector(mode = "numeric", length = length(radius))
+for (i in 1:length(radius)){
+  areas[i] <- pi * radius[i] ^ 2
+}
+areas
+
+download.file("http://www.datacarpentry.org/semester-biology/data/locations.zip", "data_raw/locations.zip")
+unzip("data_raw/locations.zip", exdir = "data_raw")
+
+data_files <- list.files(path = "data_raw", pattern = "locations-*", full.names = TRUE)
+
+for (data_file in data_files) {
+  data <- read.csv(data_file)
+  count <- nrow(data)
+  print(count)
+}
